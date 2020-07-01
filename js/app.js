bc3c71c171dcdb11ce2060433519dec2cc38ff23
Sheikh Rashed <mdrashed1310@gmail.com>
2020-07-01 21:25:44 +0600

    Version 3

--- js/app.js
+++ js/app.js
@@ -11,3 +11,37 @@ $('.news_slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
 });
+
+
+// const cal = document.querySelector('.calculator');
+// const calculator = cal.children[1];
+// const clsBtn = document.querySelector('.close');
+var btn = document.querySelector('.calculator');
+var calculator = document.querySelector('.calculator_box');
+var clsBtn = calculator.children[0].children[0].children[0];
+console.log(btn);
+console.log(calculator);
+console.log(clsBtn);
+
+btn.addEventListener('click',(e)=>{
+    
+    calculator.classList.add('open');
+});
+clsBtn.addEventListener('click',(e)=>{
+    // calculator.remove();
+    // calculator.classList.remove('open');
+   calculator.remove();
+});
+
+// $(window).load(function () {
+// 	$(".calculator").click(function(){
+// 	   $('.calculator_box').show();
+// 	});
+// 	$('.hover_bkgr_fricc').click(function(){
+// 		$('.hover_bkgr_fricc').hide();
+// 	});
+// 	$('.popupCloseButton').click(function(){
+// 		$('.hover_bkgr_fricc').hide();
+// 	});
+// });
+
